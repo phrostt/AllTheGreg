ff5085b0bc8c4c1478b1938f161d5f1edec36123
phrostt <izek@comcast.net>
2026-07-24 14:32:38 -0400

    Delete kubejs/backup directory

--- kubejs/backup/server_scripts/Gregification/quarryPlus.js
+++
@@ -1,7 +0,0 @@
-ServerEvents.recipes(allthemods => {
-
-    
-    allthemods.shapeless("quarryplus:marker", ["quarryplus:flex_marker"]);  
-    allthemods.shapeless("quarryplus:flex_marker", ["quarryplus:marker"]);  
-    
-})
