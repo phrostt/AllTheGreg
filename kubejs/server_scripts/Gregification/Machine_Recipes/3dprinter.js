3b43e7af027fb04e121b7081a742b9e9807d3460
ghorrocks217 <ghorrocks217@gmail.com>
2026-08-02 18:27:18 -0400

    fixed some errors and added recipe to water purification multi controller

--- kubejs/server_scripts/Gregification/Machine_Recipes/3dprinter.js
+++ kubejs/server_scripts/Gregification/Machine_Recipes/3dprinter.js
@@ -11,7 +11,7 @@ ServerEvents.recipes(allthemods => {
             '8x gtceu:empowered_enori_dust'
         ])
         //here
-        .itemOutputs('gtceu:empowered_polymer_dust')
+        .itemOutputs('gtceu:empowered_polymer_lens')
         .duration(600)
         .EUt(524288);
 
@@ -22,7 +22,7 @@ ServerEvents.recipes(allthemods => {
             '8x gtceu:exquisite_black_quartz_gem',
             'gtceu:plastic_singularity'
         ])
-        .itemOutputs('gtceu:chaos_plastic_dust')
+        .itemOutputs('gtceu:chaos_plastic_lens')
         .duration(600)
         .EUt(524288);
 });
--- kubejs/server_scripts/gregification.js
+++ kubejs/server_scripts/gregification.js
@@ -1,5 +1,13 @@
 ServerEvents.recipes(allthemods => {
-    
-	
-
+    allthemods.shaped('gtceu:water_filtration_plant', [
+		'FCF',
+		'PHP',
+		'FCF'
+	],
+    {
+        P: 'gtceu:hv_electric_pump', // pump
+        F: 'gtceu:stainless_steel_normal_fluid_pipe', // pipe
+        H: 'gtceu:hv_machine_hall', // machine hall
+        C: '#gtceu:circuits/hv' // circuit
+    }).id('gtceu:shaped/water_filtration_plant')
 });
